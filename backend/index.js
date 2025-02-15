275dd27199450d8b41ff6bbbff4b9d3a85380e43
Harishankar Banjare <harishankarlikeyou@gmail.com>
2025-02-15 17:34:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-backend-2v0t.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
