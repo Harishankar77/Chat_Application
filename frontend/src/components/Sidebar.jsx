4ae207a42a5157b51081a6c5b0a978f10b7fc2a5
Harishankar Banjare <harishankarlikeyou@gmail.com>
2025-02-15 17:39:37 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
     const logoutHandler = async () => {
         try {
-            const res = await axios.get(`${BASE_URL}/api/v1/user/logout`);
+            const res = await axios.get(`https://chat-backend-2v0t.onrender.com/api/v1/user/logout`);
             navigate("/login");
             toast.success(res.data.message);
             dispatch(setAuthUser(null));
